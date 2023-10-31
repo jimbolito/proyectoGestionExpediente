8c71e931a1204999971ea553a6e4fe8d99d69db6
Jimmy Coque <jimmy.coque@sumapositiva.es>
2023-10-31 15:18:54 +0100

    managementStatus model created

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,3 +2,4 @@ export * from './expedient.model';
 export * from './group.model';
 export * from './field-identifier.model';
 export * from './entity-original.model';
+export * from './management-status.model';
---
+++ src/models/management-status.model.ts
@@ -0,0 +1,58 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class ManagementStatus extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+  })
+  name?: string;
+
+  @property({
+    type: 'string',
+  })
+  code?: string;
+
+  @property({
+    type: 'string',
+  })
+  shortDescription?: string;
+
+  @property({
+    type: 'string',
+  })
+  longDescription?: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  createAt: string;
+
+  @property({
+    type: 'date',
+  })
+  deleteAt?: string;
+
+  @property({
+    type: 'date',
+  })
+  modifyAt?: string;
+
+
+  constructor(data?: Partial<ManagementStatus>) {
+    super(data);
+  }
+}
+
+export interface ManagementStatusRelations {
+  // describe navigational properties here
+}
+
+export type ManagementStatusWithRelations = ManagementStatus & ManagementStatusRelations;
