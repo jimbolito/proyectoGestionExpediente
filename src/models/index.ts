8683778cc64cb465a6cfcb5443ea9d318a1a2e3d
Jimmy Coque <jimmy.coque@sumapositiva.es>
2023-10-31 15:23:26 +0100

    globalStatusManagement model created

--- src/models/index.ts
+++ src/models/index.ts
@@ -4,3 +4,4 @@ export * from './field-identifier.model';
 export * from './entity-original.model';
 export * from './management-status.model';
 export * from './global-status-management.model';
+export * from './global-coherence.model';
