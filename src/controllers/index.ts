c1f0f37642f1cd057ea3988683920caea4b4521f
Jimmy Coque <jimmy.coque@sumapositiva.es>
2023-11-03 13:38:16 +0100

    main IntevenerController created

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -5,3 +5,4 @@ export * from './expedient-type.controller';
 export * from './expedient-client-status.controller';
 export * from './client-status-controller.controller';
 export * from './expedient-main-intervener.controller';
+export * from './main-intervener-controller.controller';
