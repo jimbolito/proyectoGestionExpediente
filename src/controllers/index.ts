70f138b59f47f802294b01122ca29a6a8c8b2e31
Jimmy Coque <jimmy.coque@sumapositiva.es>
2023-11-03 12:12:47 +0100

    created client-status-controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -3,3 +3,4 @@ export * from './expedient-expedient-type.controller';
 export * from './expedient.controller';
 export * from './expedient-type.controller';
 export * from './expedient-client-status.controller';
+export * from './client-status-controller.controller';
